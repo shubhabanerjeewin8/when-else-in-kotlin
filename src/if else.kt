a01e5483b9c88293d72a763d9196746aff8e5181
Shubha <shubhabanerjeewin8@gmail.com>
2019-04-23 19:08:27 +0530

    if and else

---
+++ src/if else.kt
@@ -0,0 +1,13 @@
+fun main (){
+
+    val x=3  //get a value of x which is not changeable
+
+    when (x){
+        1 -> println("X is 1")
+        2 -> println("X is 2")
+        3 -> println("X is 4") // here is 4 because i want to show u that in the bracket of println is only print means show result code.
+        else -> println("Value is not true")
+    }
+
+
+}
